36d01a80245eac8ba94f0521f6b6056d928fe383
Dumar Basto <dumaryekselbm@gmail.com>
2021-04-19 00:28:52 -0500

    Se dejan funcionadon las pruebas funcionales con espresso y se agrega una sencilla, la cual muestra los post del ultimo usuario

--- app/src/androidTest/java/co/com/ceiba/mobile/pruebadeingreso/view/Calificador.kt
+++ app/src/androidTest/java/co/com/ceiba/mobile/pruebadeingreso/view/Calificador.kt
@@ -2,28 +2,33 @@ package co.com.ceiba.mobile.pruebadeingreso.view
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import co.com.ceiba.mobile.pruebadeingreso.R
+import co.com.ceiba.mobile.pruebadeingreso.view.post.PostAdapter
 import co.com.ceiba.mobile.pruebadeingreso.view.user.MainActivity
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers
-import org.hamcrest.TypeSafeMatcher
+import co.com.ceiba.mobile.pruebadeingreso.view.user.UserAdapter
+import org.hamcrest.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class Calificador {
-    @Rule
-    val mActivityTestRule = ActivityScenarioRule(
+    @get:Rule
+    val mActivityTestRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(
         MainActivity::class.java
     )
 
@@ -31,32 +36,32 @@ class Calificador {
     fun emptyTest() {
         clickInputSearch()
         keypressInputSearch("empty")
-        Espresso.onView(Matchers.allOf(ViewMatchers.withText("List is empty")))
-            .check(ViewAssertions.matches(ViewMatchers.withText("List is empty")))
+        Espresso.onView(Matchers.allOf(withText("List is empty")))
+            .check(matches(withText("List is empty")))
     }
 
     @Test
     fun ervinTest() {
         clickInputSearch()
         keypressInputSearch("Ervin")
-        Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.name)))
-            .check(ViewAssertions.matches(ViewMatchers.withText("Ervin Howell")))
-        Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.phone)))
-            .check(ViewAssertions.matches(ViewMatchers.withText("010-692-6593 x09125")))
-        Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.email)))
-            .check(ViewAssertions.matches(ViewMatchers.withText("Shanna@melissa.tv")))
+        Espresso.onView(Matchers.allOf(withId(R.id.name)))
+            .check(matches(withText("Ervin Howell")))
+        Espresso.onView(Matchers.allOf(withId(R.id.phone)))
+            .check(matches(withText("010-692-6593 x09125")))
+        Espresso.onView(Matchers.allOf(withId(R.id.email)))
+            .check(matches(withText("Shanna@melissa.tv")))
     }
 
     @Test
     fun leanneTest() {
         clickInputSearch()
         keypressInputSearch("Leanne")
-        Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.name)))
-            .check(ViewAssertions.matches(ViewMatchers.withText("Leanne Graham")))
-        Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.phone)))
-            .check(ViewAssertions.matches(ViewMatchers.withText("1-770-736-8031 x56442")))
-        Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.email)))
-            .check(ViewAssertions.matches(ViewMatchers.withText("Sincere@april.biz")))
+        Espresso.onView(Matchers.allOf(withId(R.id.name)))
+            .check(matches(withText("Leanne Graham")))
+        Espresso.onView(Matchers.allOf(withId(R.id.phone)))
+            .check(matches(withText("1-770-736-8031 x56442")))
+        Espresso.onView(Matchers.allOf(withId(R.id.email)))
+            .check(matches(withText("Sincere@april.biz")))
     }
 
     @Test
@@ -64,39 +69,39 @@ class Calificador {
     fun leannePostClickVerPublicacionesTest() {
         clickInputSearch()
         keypressInputSearch("Leanne")
-        Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.btn_view_post)))
-            .perform(ViewActions.click())
+        Espresso.onView(Matchers.allOf(withId(R.id.btn_view_post)))
+            .perform(click())
         Thread.sleep(4000)
-        Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.name))).check(
-            ViewAssertions.matches(
-                ViewMatchers.withText(
+        Espresso.onView(Matchers.allOf(withId(R.id.name))).check(
+            matches(
+                withText(
                     Matchers.endsWith("Leanne Graham")
                 )
             )
         )
-        Espresso.onView(Matchers.allOf(ViewMatchers.withId(R.id.phone)))
-            .check(ViewAssertions.matches(ViewMatchers.withText("1-770-736-8031 x56442")))
+        Espresso.onView(Matchers.allOf(withId(R.id.phone)))
+            .check(matches(withText("1-770-736-8031 x56442")))
         Espresso.onView(
             Matchers.allOf(
-                ViewMatchers.withId(R.id.title),
-                ViewMatchers.withText("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")
+                withId(R.id.title),
+                withText("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")
             )
         )
-            .check(ViewAssertions.matches(ViewMatchers.withText("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")))
+            .check(matches(withText("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")))
     }
 
     private fun keypressInputSearch(valueToWrite: String) {
         val appCompatEditText2 = Espresso.onView(
             Matchers.allOf(
-                ViewMatchers.withId(R.id.editTextSearch),
+                withId(R.id.editTextSearch),
                 childAtPosition(
                     childAtPosition(
-                        ViewMatchers.withId(R.id.textInputLayoutSearch),
+                        withId(R.id.textInputLayoutSearch),
                         0
                     ),
                     0
                 ),
-                ViewMatchers.isDisplayed()
+                isDisplayed()
             )
         )
         appCompatEditText2.perform(
@@ -108,18 +113,18 @@ class Calificador {
     private fun clickInputSearch() {
         val appCompatEditText = Espresso.onView(
             Matchers.allOf(
-                ViewMatchers.withId(R.id.editTextSearch),
+                withId(R.id.editTextSearch),
                 childAtPosition(
                     childAtPosition(
-                        ViewMatchers.withId(R.id.textInputLayoutSearch),
+                        withId(R.id.textInputLayoutSearch),
                         0
                     ),
                     0
                 ),
-                ViewMatchers.isDisplayed()
+                isDisplayed()
             )
         )
-        appCompatEditText.perform(ViewActions.click())
+        appCompatEditText.perform(click())
     }
 
     companion object {
@@ -139,5 +144,41 @@ class Calificador {
                 }
             }
         }
+
+        private fun customActionClick(id: Int): ViewAction {
+            return object : ViewAction {
+                override fun getConstraints(): Matcher<View>? {
+                    return null
+                }
+
+                override fun getDescription(): String {
+                    return "Action Description"
+                }
+
+                override fun perform(uiController: UiController, view: View) {
+                    val v = view.findViewById<View>(id) as View
+                    v.performClick()
+                }
+            }
+        }
+    }
+
+    @Test
+    fun lastUserPostTest() {
+        Espresso.closeSoftKeyboard()
+        Thread.sleep(2000)
+        Espresso.onView(withId(R.id.recyclerViewSearchResults))
+            .perform(
+                RecyclerViewActions.scrollToPosition<UserAdapter.UserHolder>(Constants.LAST_POSITION),
+                RecyclerViewActions.actionOnItemAtPosition<UserAdapter.UserHolder>(
+                    Constants.LAST_POSITION,
+                    customActionClick(R.id.btn_view_post)
+                )
+            )
+        Thread.sleep(4000)
+        Espresso.onView(withId(R.id.recyclerViewPostsResults))
+            .perform(
+                RecyclerViewActions.scrollToPosition<PostAdapter.PostHolder>(Constants.LAST_POSITION),
+            )
     }
 }
--- app/src/androidTest/java/co/com/ceiba/mobile/pruebadeingreso/view/Constants.kt
+++ app/src/androidTest/java/co/com/ceiba/mobile/pruebadeingreso/view/Constants.kt
@@ -2,4 +2,5 @@ package co.com.ceiba.mobile.pruebadeingreso.view
 
 object Constants {
     const val TIME_OUT = 10000
+    const val LAST_POSITION = 9
 }
